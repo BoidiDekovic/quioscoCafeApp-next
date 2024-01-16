3bb2516f5537a14236ad00af95f0ef3ae23ce3b1
Martin <boidi.martinalejandro@gmail.com>
2024-01-16 13:24:35 -0300

    proyecto termiando

---
+++ pages/resumen.js
@@ -0,0 +1,29 @@
+import Layout from "../layout/layout"
+import useQuiosco from "../hooks/useQuiosco"
+import ResumenProducto from "../components/ResumenProducto";
+
+
+export default function Resummen() {
+    const { pedido } = useQuiosco();
+
+
+    return(
+        <Layout pagina='Resumen'> 
+            <h1 className="text-4xl font-black">Resumen</h1>
+            <p className="text-3xl mt-10">Revisa tu pedido</p>
+
+            {pedido.lenght ===0 ? (
+                <p className="text-center text=2xl">No hay elementos en tu pedido</p>
+            ) : (
+                pedido.map((producto) => (
+                    <ResumenProducto 
+                        key={producto.id}
+                        producto={producto}
+                    />
+                ))
+            )}
+        </Layout>
+    )
+
+    
+}
